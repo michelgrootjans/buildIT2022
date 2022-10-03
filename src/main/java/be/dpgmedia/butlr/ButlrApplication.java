14a63ead4f21c4fb7542f58f11c92d40bb964700
Michel Grootjans <michel.grootjans@gmail.com>
2022-10-03 09:37:09 +0200

    setup

---
+++ src/main/java/be/dpgmedia/butlr/ButlrApplication.java
@@ -0,0 +1,13 @@
+package be.dpgmedia.butlr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButlrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ButlrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/dpgmedia/butlr/ButlrApplicationTests.java
@@ -0,0 +1,13 @@
+package be.dpgmedia.butlr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButlrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
